67aa1d1a5c4e07fbce457a500920bbfbc94a2952
Urcodingbuddy <anshpethe110@gmail.com>
2024-09-25 01:15:57 +0530

    Scrolling text

--- Part-7/script.js
+++ Part-7/script.js
@@ -1,21 +1,20 @@
-
-
-window.addEventListener('wheel',function(dets){
-    if(dets.deltaY>0){
-        gsap.to('#marque',{
-            // x:'-200%',
-            transform:"translateX(-200%)",
-            repeat:-1,
-            duration:4,
-            ease:"none"
-        })
-        gsap.to('#marque img',{
-            rotate:180,
-            duration:0.5,
-        })
-    }else{
-        gsap.to('#marque',{
-            // x:'0%',
+function wheenX(){
+    window.addEventListener('wheel',function(dets){
+        if(dets.deltaY>0){
+            gsap.to('#marque',{
+                // x:'-200%',
+                transform:"translateX(-200%)",
+                repeat:-1,
+                duration:4,
+                ease:"none"
+            })
+            gsap.to('#marque img',{
+                rotate:180,
+                duration:0.5,
+            })
+        }else{
+            gsap.to('#marque',{
+                // x:'0%',
             transform:"translate(0%)",
             repeat:-1,
             duration:4,
@@ -27,3 +26,5 @@ window.addEventListener('wheel',function(dets){
         })
     }
 })
+}
+wheenX();
